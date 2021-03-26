9710a2f41a25af17d0ced9367fa9fdd548d5e2bb
Daniel Browne <daniel.browne@pusher.com>
2021-03-26 10:52:13 +0000

    Explicitly declare Swift language version support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -28,5 +28,6 @@ let package = Package(
         .testTarget(
             name: "APIotaTests",
             dependencies: ["APIota"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
