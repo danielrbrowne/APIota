275131bf49bcb497e840dd2d98de2d2eb51cda41
Daniel Browne <daniel.browne@pusher.com>
2021-03-26 15:01:16 +0000

    Remove watchOS as a platform in 'Package.swift'

--- Package.swift
+++ Package.swift
@@ -7,8 +7,7 @@ let package = Package(
     name: "APIota",
     platforms: [.iOS("13.0"),
                 .macOS("10.15"),
-                .tvOS("13.0"),
-                .watchOS("6.0")],
+                .tvOS("13.0")],
     products: [
         .library(
             name: "APIota",
