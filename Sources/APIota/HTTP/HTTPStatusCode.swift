f70c8bb92e24ab688886a2502c5365c71eb58d61
Daniel Browne <daniel.browne@pusher.com>
2021-04-06 14:04:05 +0100

    Add SwiftLint 'identifier_name' rule exceptions

--- Sources/APIota/HTTP/HTTPStatusCode.swift
+++ Sources/APIota/HTTP/HTTPStatusCode.swift
@@ -67,7 +67,6 @@ public enum HTTPStatusCode: Int {
     case processing = 102
     case earlyHints = 103
 
-    // swiftlint:disable:next identifier_name
     case ok = 200
     case created = 201
     case accepted = 202
